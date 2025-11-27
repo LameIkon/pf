328a541d8309f6ea40affa0bf3e6ff37902cd2fe
chaplinchap <jonas.linnemann@live.dk>
2025-11-27 11:40:24 +0100

    Update main-server.js

--- main-server.js
+++ main-server.js
@@ -19,6 +19,6 @@ app.use("/api/temp", tempRouter);
 
 tempSaver.fetchAPIAStore().catch((err) => {console.log("Could not fetch temperature and store data")});
 
-setInterval(tempSaver.fetchAPIAStore, 900000); // long polling, should be 15 min, as this is the time it takes for the API to update.
+setInterval(tempSaver.fetchAPIAStore, 900000); // long polling, is 15 min, as this is the time it takes for the API to update.
 
 app.listen("3000");
